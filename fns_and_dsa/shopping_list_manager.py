fb2025b9b699f666ac69d64e714099863dc64afd
Abdulrhman Ahmed <117119095+0xRudee@users.noreply.github.com>
2025-09-02 07:53:58 +0000

    Edit simple error

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,7 +1,7 @@
 import os
 
 shopping_list = []
-def menu():
+def display_menu():
     print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
@@ -11,21 +11,24 @@ def menu():
 def main():
     shopping_list = []
     while True:
-        menu()
+        display_menu()
         choice = input("Enter your choice: ")
 
         if choice == '1':
             os.system('cls')
             item = str(input("What is the item you wish to add: "))
+            if (not item.isalpha()) and (not isinstance(item, str)):
+              print(f"The item '{item}' isn't a product")
+              continue
             shopping_list.append(item)
 
         elif choice == '2':
             os.system('cls')
             item = str(input("What is the item you wish to remove: "))
-            if item in shopping_list:
-              shopping_list.remove(item)
-            else:
-                print(f"The item '{item}' isn't in you shopping list")
+            if item not in shopping_list:
+              print(f"The item '{item}' isn't in you shopping list")
+              continue
+            shopping_list.remove(item)
 
         elif choice == '3':
             os.system('cls')
