48031485a6ee3b3b8dff1945a8e741bacd0b26e7
Abdulrhman Ahmed <117119095+0xRudee@users.noreply.github.com>
2025-09-02 07:01:43 +0000

    asd

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,11 @@
 def perform_operation(num1, num2, operation):
-  match operation :
-    case 'add' :
-      return num1 + num2
-    case 'subtract' :
-      return num1 - num2
-    case 'multiply' :
-      return num1 * num2
-    case 'divide' :
-      if num2 == 0:
-        return None
-      else:
-        return num1 / num2
+
+if operation == 'add':
+    return num1 + num2
+elif operation == 'subtract':
+    return num1 - num2
+elif operation == 'multiply':
+    return num1 * num2
+elif operation == 'divide':
+    return None if num2 == 0 else num1 / num2
+
